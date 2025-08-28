3cb11f61b12a332d03023fb1bc173ea38e5c6f72
MishaDakhnenko <114470203+MishaDakhnenko@users.noreply.github.com>
2025-08-28 22:28:46 +0300

    Update lesson4_.DML_OPTIONAL.sql

--- HW 4/lesson4_.DML_OPTIONAL.sql
+++ HW 4/lesson4_.DML_OPTIONAL.sql
@@ -1,4 +1,24 @@
-INSERT INTO EmployeeProjects (EmployeeID, ProjectID, HoursWorked)
-VALUES (6, 1, 80);
+-- Вставим двух новых сотрудников
+INSERT INTO Employees (FirstName, LastName, Department, Salary)
+VALUES ('Bob', 'Dylan', 'IT', 70000.00),
+       ('Johnny', 'Cash', 'Finance', 90000.00);
 
-COMMIT;
+-- Выберем всех сотрудников
+SELECT * FROM Employees;
+
+-- Выберем только FirstName и LastName сотрудников из отдела 'IT'
+SELECT FirstName, LastName
+FROM Employees
+WHERE Department = 'IT';
+
+-- Обновим Salary 'Alice Smith' до 65000.00
+UPDATE Employees
+SET Salary = 65000.00
+WHERE FirstName = 'Alice' AND LastName = 'Smith';
+
+-- Удалим сотрудника, чья LastName = 'Prince'
+DELETE FROM Employees
+WHERE LastName = 'Prince';
+
+-- Проверим все изменения
+SELECT * FROM Employees;
