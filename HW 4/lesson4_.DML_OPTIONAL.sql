20243b89add145f6925e7e1a7847b7d74a4b5588
MishaDakhnenko <114470203+MishaDakhnenko@users.noreply.github.com>
2025-08-28 22:24:34 +0300

    Add files via upload

---
+++ HW 4/lesson4_.DML_OPTIONAL.sql
@@ -0,0 +1,4 @@
+INSERT INTO EmployeeProjects (EmployeeID, ProjectID, HoursWorked)
+VALUES (6, 1, 80);
+
+COMMIT;
---
+++ HW 4/lesson4_DML-DCL.sql
@@ -0,0 +1,4 @@
+INSERT INTO Employeeprojects (Employeeid, ProjectID, Hoursworked)
+values (1,4, 120), (2, 4, 90);
+
+commit;
